4fcc523c6f3accaaa1acdda640b4e78009e8eb5d
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-18 10:29:36 -0700

    adds initial setup for fizzBuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,5 @@
 function fizzBuzz() {
-  var fizzedBuzzedArray = [
+  var numbersArray = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
@@ -12,6 +12,9 @@ function fizzBuzz() {
     91, 92, 93, 94, 95, 96, 97, 98, 99, 100
   ]
 }
+
+//filter
+//forEach
 
 module.exports = fizzBuzz
 
