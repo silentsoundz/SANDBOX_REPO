5e8452afc423213263cf89caf6cf853b0adb2776
Monica Williams <monicaestellaw@gmail.com>
2017-04-17 17:48:12 -0700

    adds all tests for fizzbuzz

--- test/fizzBuzz_test.js
+++ test/fizzBuzz_test.js
@@ -7,14 +7,20 @@ describe( 'fizzBuzz()', function(){
     expect( fizzBuzz ).to.be.a( 'function' )
   })
 
-  it( 'returns an object with all coin types (quarters, dimes, nickels, and pennies )', function(){
-    const change = fizzBuzz({ price: 100, amountGiven: 100 })
-    expect( change ).to.be.an( 'object' )
-    expect( change ).to.have.keys( 'quarters', 'dimes', 'nickels', 'pennies' )
-    expect( change ).to.deep.equal({
-      quarters: 0,
-      dimes: 0,
-      nickels: 0,
-      pennies: 0,
-    })
+  it( 'returns an array of numbers from 1 to 100 where multiples of three are replaced with Fizz, multiples of five are replaced with Buzz, and multiples of both are replaced with FizzBuzz', function(){
+    var fizzedBuzzedArray = [
+      1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz',
+      11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz',
+      'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz',
+      31, 32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38, 'Fizz', 'Buzz',
+      41, 'Fizz', 43, 44, 'FizzBuzz', 46, 47, 'Fizz', 49, 'Buzz',
+      'Fizz', 52, 53, 'Fizz', 'Buzz', 56, 'Fizz', 58, 59, 'FizzBuzz',
+      61, 62, 'Fizz', 64, 'Buzz', 'Fizz', 67, 68, 'Fizz', 'Buzz',
+      71, 'Fizz', 73, 74, 'FizzBuzz', 76, 77, 'Fizz', 79, 'Buzz',
+      'Fizz', 82, 83, 'Fizz', 'Buzz', 86, 'Fizz', 88, 89, 'FizzBuzz',
+      91, 92, 'Fizz', 94, 'Buzz', 'Fizz', 97, 98, 'Fizz', 'Buzz'
+    ]
+
+    expect( fizzBuzz()).to.equal( fizzedBuzzedArray )
   })
+})
