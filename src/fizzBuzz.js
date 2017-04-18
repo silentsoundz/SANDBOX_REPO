d0934976561ceffe4ef07e8bd31af51aef3a2dbe
Monica Williams <monicaestellaw@gmail.com>
2017-04-17 17:09:54 -0700

    starts fizzbuzz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,18 @@
+function fizzBuzz() {
+  var fizzedBuzzedArray = [
+  ]
+}
+
+
+#### fizzBuzz
+
+Return an array of numbers from 1 to 100.
+
+For multiples of three, use the string `Fizz` instead of the number and for multiples of five replace with `Buzz`.
+
+For numbers which are multiples of both three and five replace with `FizzBuzz`.
+
+```javascript
+fizzBuzz()
+// => [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', ...]
+```
