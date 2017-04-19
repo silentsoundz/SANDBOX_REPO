840b37f300ef02ce8fa56884b26ec2230a4db0e8
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-19 12:16:30 -0700

    finishes test and implementation files for collatzconjuncture; all tests pass

--- src/collatzConjecture.js
+++ src/collatzConjecture.js
@@ -1,40 +1,20 @@
-function collatzConjecture( num ) {
-  var collatzArray= []
-
+function collatzConjecture(num) {
   if ( typeof num !== 'number' ) {
-    return 'You\'re not a number'
+    return ('You\'re not a number')
   }
-  collatzArray.push( num )
-  if ( collatzArray[collatzArray.length - 1] ===  1 ) {
-    return collatzArray
+  if ( num ===  1 ) {
+    let result = [num]
+    return result
+  } else if ( num % 2 === 0 ) {
+      //we have to hold onto the results from the prev iteration of collatzConjecture
+    let result = [num].concat(collatzConjecture( num / 2))
+    return result
+  } else {
+    let result = [num].concat(collatzConjecture( num * 3 + 1))
+    return result
   }
-//initial check to make sure that input is correct input
-  //if it isn't, return 'you're not a number'
-//push the number to the array
-//check the number at the end/top of the array
-//if it is 1
-  //return the array
-//if it is even, divide by 2
-//push that result to our array
-//if it is odd, multiply by 3, then add 1
-//push that result to our array
-
+  return collatzArray
 }
 
-console.log(collatzConjecture(1))
 
 module.exports = collatzConjecture
-// #### collatzConjecture
-//
-// Return the Collatz sequence for a given number.
-//
-// The Collatz sequence for any positive integer `n` is defined as follows:
-//
-// > If `n` is even, divide it by 2 to get `n / 2`. If `n` is odd, multiply it by 3 and add 1 to obtain `3n + 1`. Repeat the process until you reach 1.
-//
-// ```javascript
-// collatzConjecture(1)
-// // => [1]
-//
-// collatzConjecture(7)
-// // => [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
