1d9890057f7938936b69fefc00a0685a9da69d7d
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-18 17:25:49 -0700

    adds test and implementation files

---
+++ src/fibonacci.js
@@ -0,0 +1,10 @@
+
+
+
+// #### fibonacci
+//
+// Return an array of Fibonacci numbers to the _nth_ position.
+//
+// ```javascript
+// fibonacci(10)
+// // => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
---
+++ test/fibonacci_test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import fibonacci from '../src/fibonacci'
+
+describe( 'fibonacci()', function(){
+
+  it( 'should be a function', function(){
+    expect( fibonacci ).to.be.a( 'function' )
+  })
+
+  it( 'Returns an array of Fibonacci numbers to the _nth_ position.', function(){
+    expect( fibonacci(10)).to.equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+  })
+  it( 'Returns an array of Fibonacci numbers to the _nth_ position.', function(){
+    expect( fibonacci(15)).to.equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+  })
+  it( 'Returns an array of Fibonacci numbers to the _nth_ position.', function(){
+    expect( fibonacci('I want to be a number')).to.equal('Your not a number')
+  })
+})
