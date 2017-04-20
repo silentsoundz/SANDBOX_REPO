5a5072978412ae55b93042fd50a71470c0c8fdff
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-19 17:18:54 -0700

    makes changes per Ethan review

--- src/setUnion.js
+++ src/setUnion.js
@@ -1,4 +1,5 @@
 function setUnion( array1, array2 ) {
+var resultArray = array1
 
   if ( !Array.isArray( array1 ) || !Array.isArray( array2 )) {
      return 'This is not an array, fool'
@@ -7,14 +8,13 @@ function setUnion( array1, array2 ) {
   for ( var index = 0; index < array1.length; index++ ) {
     for ( var jindex = 0; jindex < array2.length; jindex++ ) {
       if ( array1[index] ===  array2[jindex] ) {
-        array1.splice( index, 1 )
+        resultArray.splice( index, 1 )
       }
     }
   }
-  var finalArray = array1.concat(array2)
-  return finalArray.sort()
+  return resultArray.concat(array2).sort()
 }
 
-console.log(setUnion([1, 2, 3, 4,], [2, 4, 6, 8,]));
+console.log(setUnion([1, 2, 3, 4], [2, 4, 6, 8]));
 
 module.exports = setUnion
