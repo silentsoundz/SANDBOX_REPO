f2b7fb0b46e26c1ff8dd6af9dd7e48ac3cd2f79a
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-20 09:37:25 -0700

    makes changes per ethan review

--- src/setIntersection.js
+++ src/setIntersection.js
@@ -1,14 +1,14 @@
-function setIntersection( arr1, arr2 ) {
+function setIntersection( array1, array2 ) {
   var resultArray = []
 
-  if (( !arr1 || !arr2 ) ||
-      (!Array.isArray( arr1 ) || !Array.isArray( arr1 ))) {
+  if (( !array1 || !array2 ) ||
+      (!Array.isArray( array1 ) || !Array.isArray( array1 ))) {
     return 'Not a valid input.'
   }
-  for ( var index = 0; index < arr1.length; index++ ) {
-    for ( var jindex = 0; jindex < arr2.length; jindex++ ) {
-      if ( arr1[index] === arr2[jindex] ) {
-        resultArray.push( arr1[index] )
+  for ( var index = 0; index < array1.length; index++ ) {
+    for ( var jindex = 0; jindex < array2.length; jindex++ ) {
+      if ( array1[index] === array2[jindex] ) {
+        resultArray.push( array1[index] )
       }
     }
   }
--- test/setIntersection_test.js
+++ test/setIntersection_test.js
@@ -13,6 +13,9 @@ describe( 'setIntersection()', function(){
   it( 'returns the intersection of two sets.', function(){
     expect( setIntersection([1, 5, 3, 37], [37, 4, 8, 6])).to.eql([37])
   })
+  it( 'returns an empty array if there is no intersection.', function(){
+    expect( setIntersection([1, 5, 3, 37], [7, 4, 8, 6])).to.eql([])
+  })
   it( 'only operates on two arrays.', function(){
     expect( setIntersection([1, 2, 3, 4])).to.eql('Not a valid input.')
   })
