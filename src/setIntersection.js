598a2c4735ccfa37df2eca9b50b10798bec66e42
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-19 17:45:22 -0700

    finishes setIntersection tests and implementation file; all tests pass

--- src/setIntersection.js
+++ src/setIntersection.js
@@ -5,29 +5,14 @@ function setIntersection( arr1, arr2 ) {
       (!Array.isArray( arr1 ) || !Array.isArray( arr1 ))) {
     return 'Not a valid input.'
   }
-
-  //if there is only one parameter or if either input is not an array
-    //return 'Not a valid input.'
-  //sort the arrays ( when we have merge sort writte )
-  //loop through 1
-    //loop through 2
-      //if 1[index] === 2[index]
-        //arr.push 1[index]
-  //return arr
+  for ( var index = 0; index < arr1.length; index++ ) {
+    for ( var jindex = 0; jindex < arr2.length; jindex++ ) {
+      if ( arr1[index] === arr2[jindex] ) {
+        resultArray.push( arr1[index] )
+      }
+    }
+  }
   return resultArray
 }
 
-console.log(setIntersection([1, 2, 3, 4], [2, 4, 6, 8]))
-
 module.exports = setIntersection
-
-// #### setIntersection
-//
-// Return the intersection of two sets.
-
-// ```javascript
-// const a = [1, 2, 3, 4]
-// const b = [2, 4, 6, 8]
-// setIntersection(a, b)
-// // => [2, 4]
-// ```
--- test/setIntersection_test.js
+++ test/setIntersection_test.js
@@ -20,14 +20,3 @@ describe( 'setIntersection()', function(){
     expect( setIntersection('hi')).to.eql('Not a valid input.')
   })
 })
-
-// #### setIntersection
-//
-// Return the intersection of two sets.
-//
-// ```javascript
-// const a = [1, 2, 3, 4]
-// const b = [2, 4, 6, 8]
-// setIntersection(a, b)
-// // => [2, 4]
-// ```
