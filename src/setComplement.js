1334064002ab800905641501f64aab739fde045c
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-20 11:20:08 -0700

    makes changes per frankie review

--- src/setComplement.js
+++ src/setComplement.js
@@ -6,11 +6,10 @@
 
 //ES6 implementation
 const setComplement = ( array1, array2 ) => {
-
-  if ((!Array.isArray( array1 ) || !Array.isArray( array2 ))
-    || ( !array1 || !array2 )) {
+  if ( !Array.isArray( array1 ) || !Array.isArray( array2 ) ) {
       return 'Not a valid input.'
   }
+
   return array2.filter( x => array1.indexOf(x) === -1 )
 }
 
