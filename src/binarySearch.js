79eb76b3ed4948c51744a52c14149e95f99ab87d
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-20 17:55:40 -0700

    this works but looks kind of weird

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,6 +1,31 @@
-function binarySearch() {
+function binarySearch( arr, number ) {
+  var midpoint = Math.floor( arr.length/2 - 1 )
+  var minIndex = 0
+  var maxIndex = arr.length - 1
 
+  while ( minIndex !== maxIndex ) {
+    if( arr[midpoint] === number ) {
+      return midpoint
+    } else if ( arr[midpoint] < number ) {
+      minIndex = midpoint + 1
+      midpoint = Math.floor((minIndex + maxIndex)/2)
+    } else if ( arr[midpoint] > number ) {
+      maxIndex = midpoint - 1
+      midpoint = Math.floor((minIndex + maxIndex)/2)
+    }
+  }
+  if( arr[midpoint] === number ) {
+    return midpoint
+  }
+  return -1
 }
+
+
+  //return stuff
+
+
+console.log( binarySearch([6,7,8,9, 10, 30, 88, 89, 100, 123], 89));
+module.exports = binarySearch
 // #### binarySearch
 //
 // Search for a number within an array using the **binary search** algorithm.
