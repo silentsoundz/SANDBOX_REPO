9072af344b6cdc4da3ba5c4a618bafe7dd75393b
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-21 09:24:43 -0700

    cleaned up binary search; all tests passing

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -3,6 +3,9 @@ function binarySearch( arr, number ) {
   var minIndex = 0
   var maxIndex = arr.length - 1
 
+  if ( !Array.isArray( arr ) ) {
+      return 'This is not an array, fool'
+  }
   while ( minIndex !== maxIndex ) {
     if( arr[midpoint] === number ) {
       return midpoint
@@ -20,19 +23,4 @@ function binarySearch( arr, number ) {
   return -1
 }
 
-
-  //return stuff
-
-
-console.log( binarySearch([6,7,8,9, 10, 30, 88, 89, 100, 123], 89));
 module.exports = binarySearch
-// #### binarySearch
-//
-// Search for a number within an array using the **binary search** algorithm.
-// [Binary Search](https://en.wikipedia.org/wiki/Binary_search_algorithm) looks for an item in a list by looking at a list's center, then looking at the center of the left or right half, continually dividing the search space in half until it finds what it's looking for.
-//
-// ```javascript
-// const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-// binarySearch(numbers, 45)
-// // => 8
-// ```
