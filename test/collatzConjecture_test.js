d781b0e19cfd02c5cdd200c0b8097411964b797b
Stephani McGrath <silentsoundz1977@gmail.com>
2017-06-07 13:35:23 -0700

    playing with push

--- test/collatzConjecture_test.js
+++ test/collatzConjecture_test.js
@@ -9,11 +9,4 @@ describe( 'collatzConjecture()', function(){
 
   it( 'Returns the Collatz sequence for a given number.', function(){
     expect( collatzConjecture(7)).to.eql([7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
-  })
-  it( 'Returns the Collatz sequence for a given number.', function(){
-    expect( collatzConjecture(1)).to.eql([1])
-  })
-  it( 'only takes a number as an input.', function(){
-    expect( collatzConjecture('hi')).to.eql('You\'re not a number')
-  })
-})
+
