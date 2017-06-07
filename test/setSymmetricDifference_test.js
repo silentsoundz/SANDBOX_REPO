fdec49c08785c0e1d642be7fc5918e38a1e21ba1
Stephani McGrath <silentsoundz1977@gmail.com>
2017-06-07 14:57:20 -0700

    cherry pick me too

--- test/setSymmetricDifference_test.js
+++ test/setSymmetricDifference_test.js
@@ -7,14 +7,6 @@ describe( 'setSymmetricDifference()', function(){
     expect( setSymmetricDifference ).to.be.a( 'function' )
   })
 
-  it( 'returns the complement of two sets.', function(){
-    expect( setSymmetricDifference([1, 2, 3, 4], [2, 4, 6, 8])).to.eql([1, 3, 6, 8])
-  })
-  it( 'returns the complement of two sets.', function(){
-    expect( setSymmetricDifference([1, 5, 3, 37], [37, 4, 8, 6])).to.eql([1, 5, 3, 4, 8, 6])
-  })
-  it( 'returns an empty array if there is no complement.', function(){
-    expect( setSymmetricDifference([1, 5, 3, 37], [1, 5, 3, 37])).to.eql([])
   })
   it( 'only operates on two arrays.', function(){
     expect( setSymmetricDifference([1, 2, 3, 4])).to.eql('Not a valid input.')
