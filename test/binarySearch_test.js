42c3e3f3eba98ba5e6389c19ef35e7b81a00946c
Stephani McGrath <silentsoundz1977@gmail.com>
2017-06-07 14:56:14 -0700

    cherry pick me'

--- test/binarySearch_test.js
+++ test/binarySearch_test.js
@@ -13,11 +13,6 @@ describe( 'binarySearch()', function(){
     expect( binarySearch([1, 2, 3, 4, 6, 8], 3)).to.equal(2)
   })
 
-  it( 'returns -1 if the second input does not exist in the array.', function(){
-    expect( binarySearch([1, 2, 3, 4, 6, 8], 37)).to.equal(-1)
-  })
-
-  it( 'only takes an array as a first input.', function(){
-    expect( binarySearch( 1, 2 )).to.eql('This is not an array, fool')
+ fool')
   })
 })
